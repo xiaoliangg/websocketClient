b31eee0da4b77f28b07f31adec1798f06728874f
xiaoliangg <1535523313@qq.com>
2023-04-19 18:13:11 +0800

    删除无用逻辑，无用代码

--- socketioClientDemo/src/main/java/org/example/SocketIOClientDemo.java
+++ socketioClientDemo/src/main/java/org/example/SocketIOClientDemo.java
@@ -24,54 +24,28 @@ public class SocketIOClientDemo {
     public static final String wsUri = SCHEMA_TEST + "/?capabilities=NEW_DOMAIN_SUPPORTTED%2CPAY_CONTENT_SUPPORTTED&os=android&lng=114.45123091&openid=kc34992023041210000003&packagename=com.edog.car&sign=da838eb65cd00d0f4561aa5110793943&deviceid=09bd6d56b59fa6aba99021263ec3b7e9&carType=X7&appid=kc3499&udid=09bd6d56b59fa6aba99021263ec3b7e9&lat=38.03762354";
 
     public static void main(String[] args) {
+        // charls代理
 //        System.setProperty("http.proxyHost", "127.0.0.1");
 //        System.setProperty("http.proxyPort", "8888");
 
-        // 服务端socket.io连接通信地址
-//        String url = "http://127.0.0.1:8888";
         try {
             IO.Options options = new IO.Options();
             options.transports = new String[]{ WebSocket.NAME};
-//            options.forceNew = true;
             options.reconnectionAttempts = 2;
             // 失败重连的时间间隔
             options.reconnectionDelay = 1000;
             // 连接超时时间(ms)
             options.timeout = 500;
-            // userId: 唯一标识 传给服务端存储
-//            final Socket socket = IO.socket(url + "?userId=1", options);
 
             final Socket socket = IO.socket(wsUri, options);
-
             socket.on(Socket.EVENT_CONNECT, args1 -> {
                 try {
                     System.out.println("连接建立成功");
-//                    socket.send("hello...");
                 }catch (Exception e){
                     e.printStackTrace();
                 }
             });
 
-            // 自定义事件`connected` -> 接收服务端成功连接消息
-            socket.on("connected", objects -> {
-                try {
-                    System.out.println("服务端:" + objects[0].toString());
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            });
-
-            // 自定义事件`push_data_event` -> 接收服务端消息
-//            socket.on("push_data_event", objects -> System.out.println("服务端:" + objects[0].toString()));
-
-            // 自定义事件`myBroadcast` -> 接收服务端广播消息
-            socket.on("myBroadcast", objects -> {
-                try {
-                    System.out.println("服务端myBroadcast：" + objects[0].toString());
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            });
             socket.on("appMessage", objects -> {
                 try {
                     System.out.println("服务端appMessage：" + objects[0].toString());
