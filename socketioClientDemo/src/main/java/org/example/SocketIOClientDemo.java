66662cfebb2de74a5d8a6b019f1fb80ef95ccd25
xiaoliangg <1535523313@qq.com>
2023-04-24 15:30:35 +0800

    新增namespace调试通过

--- socketioClientDemo/src/main/java/org/example/SocketIOClientDemo.java
+++ socketioClientDemo/src/main/java/org/example/SocketIOClientDemo.java
@@ -1,16 +1,19 @@
 package org.example;
 
 import io.socket.client.IO;
+import io.socket.client.Manager;
 import io.socket.client.Socket;
 import io.socket.engineio.client.EngineIOException;
 import io.socket.engineio.client.transports.WebSocket;
 
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 public class SocketIOClientDemo {
-    public static final String SCHEMA_TEST = "https://iovws-test.radio.cn";
+//    public static final String SCHEMA_TEST = "https://iovws-test.radio.cn";
+    public static final String SCHEMA_TEST = "http://iovws-dev.radio.cn/open";
 //    public static final String SCHEMA_TEST = "http://iovws-test.radio.cn";
 //    public static final String SCHEMA_TEST = "http://localhost:9095";
 
@@ -22,7 +25,7 @@ public class SocketIOClientDemo {
 //    public static final String lat = "38.03762354";
 //    public static final String lng = "114.45123091";
 
-    public static final String deviceId = "a89b0935f1aaa35984dd6924d0c416f3";
+    public static String deviceId = "a89b0935f1aaa35984dd6924d0c416f3";
     public static final String appId = "in2193";
     public static final String packagename = "com.kaolafm.sdk.demo";
     public static final String openid = "in21932023041910000001";
@@ -30,26 +33,39 @@ public class SocketIOClientDemo {
     public static final String lat = "38.03762354";
     public static final String lng = "114.45123091";
 
-    public static final String WS_URI = SCHEMA_TEST + "/?capabilities=NEW_DOMAIN_SUPPORTTED%2CPAY_CONTENT_SUPPORTTED&os=android&lng=" + lng
-            +"&openid=" + openid + "&packagename=" + packagename + "&sign="+ sign +"&deviceid=" + deviceId
-            +"&carType=X7&appid="+ appId + "&lat=" + lat;
+//    public static final String WS_URI = "http://iovws-dev.radio.cn/open?capabilities=NEW_DOMAIN_SUPPORTTED%2CPAY_CONTENT_SUPPORTTEDos=android&lng=" + lng
+//            +"&openid=" + openid + "&packagename=" + packagename + "&sign="+ sign +"&deviceid=" + deviceId
+//            +"&carType=X7&appid="+ appId + "&lat=" + lat;
+//    public static final String WS_URI = "http://iovws-dev.radio.cn/open";
+
 
     public static void main(String[] args) {
         // charls代理
-//        System.setProperty("http.proxyHost", "127.0.0.1");
-//        System.setProperty("http.proxyPort", "8888");
+        System.setProperty("http.proxyHost", "127.0.0.1");
+        System.setProperty("http.proxyPort", "8888");
 
         try {
             IO.Options options = new IO.Options();
             options.transports = new String[]{ WebSocket.NAME};
+//            options.query = "capabilities=NEW_DOMAIN_SUPPORTTED%2CPAY_CONTENT_SUPPORTTED&os=android&lng=116.354957&openid=kc34992023041210000003&packagename=com.edog.car&sign=da838eb65cd00d0f4561aa5110793943&deviceid=09bd6d56b59fa6aba99021263ec3b7e9&carType=X7&appid=kc3499&lat=39.910639";
+
+            String uriStr = "http://iovws-dev.radio.cn?capabilities=NEW_DOMAIN_SUPPORTTED%2CPAY_CONTENT_SUPPORTTED&os=android&lng=116.354957&openid=kc34992023041210000003&packagename=com.edog.car&sign=da838eb65cd00d0f4561aa5110793943&deviceid=09bd6d56b59fa6aba99021263ec3b7e9&carType=X7&appid=kc3499&lat=39.910639";
+            deviceId = "09bd6d56b59fa6aba99021263ec3b7e9";
+            URI uri = URI.create(uriStr);
+            Manager manager = new Manager(uri,options);
+            final Socket socket = manager.socket("/open");
+//            final Socket socket = IO.socket(uri,options);
+//            final Socket socket = socket2.io().socket("/open");
+//            final Socket socket = manager.socket("");
 //            options.reconnectionAttempts = 2;
 //            // 失败重连的时间间隔
 //            options.reconnectionDelay = 1000;
 //            // 连接超时时间(ms)
 //            options.timeout = 500;
 
-            System.out.println("WS_URI:" + WS_URI);
-            final Socket socket = IO.socket(WS_URI, options);
+//            System.out.println("WS_URI:" + WS_URI);
+//            final Socket socket = IO.socket(WS_URI, options);
+
             socket.on(Socket.EVENT_CONNECT, args1 -> {
                 try {
                     System.out.println("连接建立成功");
@@ -64,20 +80,21 @@ public class SocketIOClientDemo {
                     e.printStackTrace();
                 }
             });
-            socket.on(Socket.EVENT_ERROR, args1 -> {
-                try {
-                    System.out.println("连接异常");
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            });
-            socket.on(Socket.EVENT_CONNECT_TIMEOUT, args1 -> {
-                try {
-                    System.out.println("connect_timeout");
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            });
+
+//            socket.on(Socket.EVENT_ERROR, args1 -> {
+//                try {
+//                    System.out.println("连接异常");
+//                }catch (Exception e){
+//                    e.printStackTrace();
+//                }
+//            });
+//            socket.on(Socket.EVENT_CONNECT_TIMEOUT, args1 -> {
+//                try {
+//                    System.out.println("connect_timeout");
+//                }catch (Exception e){
+//                    e.printStackTrace();
+//                }
+//            });
             socket.on(Socket.EVENT_CONNECT_ERROR, args1 -> {
                 try {
                     System.out.println("connect_error:" + args1[0].toString());
@@ -94,39 +111,33 @@ public class SocketIOClientDemo {
                 }
             });
 
-            socket.on("appMessage", objects -> {
+            socket.on("EB_PUSH", objects -> {
                 try {
-                    System.out.println("服务端appMessage：" + objects[0].toString());
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            });
-            socket.on("apiEBMessage", objects -> {
-                try {
-                    System.out.println("服务端apiEBMessage：" + objects[0].toString());
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            });
-            socket.on("locationUpdateMessageResponse", objects -> {
-                try {
-                    System.out.println("服务端locationUpdateMessageResponse：" + objects[0].toString());
+                    System.out.println("EB_PUSH：" + objects[0].toString());
                 }catch (Exception e){
                     e.printStackTrace();
                 }
             });
 
+            socket.on("LOC_RESP", objects -> {
+                try {
+                    System.out.println("LOC_RESP：" + objects[0].toString());
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            });
+//            socket.io();socket.open();
             socket.connect();
 
-            // 10s后断开连接
-            new Thread(() -> {
-                try {
-                    Thread.sleep(10*1000);
-                    socket.disconnect();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }).start();
+            // 100s后断开连接
+//            new Thread(() -> {
+//                try {
+//                    Thread.sleep(100*1000);
+//                    socket.disconnect();
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }).start();
 
             while (true) {
                 Thread.sleep(3000);
@@ -139,7 +150,7 @@ public class SocketIOClientDemo {
                     map.put("requestId",uuid);
                     map.put("lat",Double.parseDouble(lat));
                     map.put("lng",Double.parseDouble(lng));
-                    socket.emit("locationUpdateMessage",map);
+                    socket.emit("LOC_RPT",map);
                 }else{
                     System.out.println("未连接状态");
                 }
